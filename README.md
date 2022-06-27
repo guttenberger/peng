22395a0ffc2f025072ce3d631cb6ce798dd8d0fe
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2022-06-27 15:43:29 +0200

    add prototypes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Advanced Privacy Friendly Access Control
+
+
+
+
+Further Resources:
+- [AWS Blogpost](https://aws.amazon.com/de/blogs/storage/managing-access-to-your-amazon-s3-objects-with-a-custom-authorizer/)
--- backend/book-cover/app.js
+++ backend/book-cover/app.js
@@ -1,4 +1,27 @@
 "use strict";
-exports.handler = (event, context, callback) => {
-    console.log();
+
+const { S3 } = require("aws-sdk");
+
+const s3 = new S3();
+
+exports.handler = async (event, context) => {
+  // Output the event details to CloudWatch Logs.
+  console.log("Event:\n", JSON.stringify(event, null, 2));
+
+  // check auth
+  // return await verifyAuth(event);
+
+  // check file exits
+}
+
+async function verifyAuth(event) {
+  // const allowAccess =
+
+  await s3.writeGetObjectResponse({
+    StatusCode: 403,
+    ErrorCode: 'NotAuthorized',
+    ErrorMessage: 'Not Authorized'
+  });
+
+  return { 'status_code': 403 }
 }
