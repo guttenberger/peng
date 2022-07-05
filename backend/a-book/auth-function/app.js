905f56e473d129ca635091d978b4fdbdf6264f45
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2022-07-05 14:34:54 +0200

    first draft privacy interceptor

--- backend/a-book/auth-function/app.js
+++ backend/a-book/auth-function/app.js
@@ -1,19 +1,61 @@
-"use strict";
+// A simple token-based authorizer example to demonstrate how to use an authorization token 
+// to allow or deny a request. In this example, the caller named 'user' is allowed to invoke 
+// a request if the client-supplied token value is 'allow'. The caller is not allowed to invoke 
+// the request if the token value is 'deny'. If the token value is 'unauthorized' or an empty
+// string, the authorizer function returns an HTTP 401 status code. For any other token value, 
+// the authorizer returns an HTTP 500 status code. 
+// Note that token values are case-sensitive.
 
-exports.handler = async (event, context) => {
+exports.handler =  async (event, context) => {
   // Output the event details to CloudWatch Logs.
   console.log("Auth-Event:\n", JSON.stringify(event, null, 2));
+  
+  const { purpose } = event.queryStringParameters;
 
-  const { purpose } = event.queryStringParameters.purpose;
+    const response = purpose === "testabc"
+      ? generatePolicy('user', 'Allow', event.methodArn)
+      : generatePolicy('user', 'Deny', event.methodArn);
 
-  const isAuthorized = purpose === "testabc";
+      // Optional output with custom properties of the String, Number or Boolean type.
+      response.context = {
+        "stringKey": "stringval",
+        "numberKey": 123,
+        "booleanKey": true
+      };
 
-  let response = {
-    isAuthorized,
-    "context": {
-      "purpose": "value",
+    console.log("Auth-Response:\n", JSON.stringify(response, null, 2));
+
+
+    return response;
+};
+
+// Help function to generate an IAM policy
+const generatePolicy = (principalId, effect, resource) => {
+    
+  let authResponse = {};
+    
+    authResponse.principalId = principalId;
+    if (effect && resource) {
+        
+      let policyDocument = {};
+        policyDocument.Version = '2012-10-17'; 
+        policyDocument.Statement = [];
+        
+        let statementOne = {};
+        statementOne.Action = 'execute-api:Invoke'; 
+        statementOne.Effect = effect;
+        statementOne.Resource = resource;
+        policyDocument.Statement[0] = statementOne;
+
+        // // todo be specific
+        // let statementTwo = {};
+        // statementTwo.Action = 's3:*'; 
+        // statementTwo.Effect = effect;
+        // statementTwo.Resource = "arn:aws:s3:::*";
+        // policyDocument.Statement[1] = statementTwo;
+
+        authResponse.policyDocument = policyDocument;
     }
-  };
 
-  return response;
+    return authResponse;
 }
--- backend/a-book/book-cover/app.js
+++ backend/a-book/book-cover/app.js
@@ -1,8 +1,24 @@
-"use strict";
+const { S3 } = require("aws-sdk");
 
-exports.handler = async (event, context) => {
+const s3 = new S3();
+
+exports.handler = async (event) => {
+  const { BUCKET_NAME } = process.env;
   // Output the event details to CloudWatch Logs.
-  console.log("Event:\n", JSON.stringify(event, null, 2));
+  console.log("BucketAccess-Event:\n", JSON.stringify(event, null, 2));
 
-  return { 'status_code': 200 }
-}
+  const path = event.requestContext.resourcePath;
+
+  const params =
+  {
+    Bucket: path, // a path to your Bucket
+    Key: 'test.png' // a key (literally a path to your file)
+  }
+
+  const result = await s3.getObject(params).promise();
+
+  console.log(result);
+
+  // Exit the Lambda function.
+  return { statusCode: 200 };
+};
