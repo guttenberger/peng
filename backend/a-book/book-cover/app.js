d879dc81e894ad74660ee8ac06eb11ecc743de39
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2022-07-05 16:38:36 +0200

    use s3 access lambda

--- backend/a-book/book-cover/app.js
+++ backend/a-book/book-cover/app.js
@@ -3,18 +3,18 @@ const { S3 } = require("aws-sdk");
 const s3 = new S3();
 
 exports.handler = async (event) => {
-  const { BUCKET_NAME } = process.env;
-  // Output the event details to CloudWatch Logs.
+    // Output the event details to CloudWatch Logs.
   console.log("BucketAccess-Event:\n", JSON.stringify(event, null, 2));
 
-  const path = event.requestContext.resourcePath;
-
+  const { S3_ACCESS_POINT } = process.env;
+  const { id } = event.pathParameters;
   const params =
   {
-    Bucket: path, // a path to your Bucket
-    Key: 'test.png' // a key (literally a path to your file)
+    Bucket: S3_ACCESS_POINT, // a path to your Bucket
+    Key: id // a key (literally a path to your file)
   }
 
+  console.log("S3Params-Event:\n", JSON.stringify({ params, path }, null, 2));
   const result = await s3.getObject(params).promise();
 
   console.log(result);
