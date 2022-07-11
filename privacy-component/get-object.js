4c7eb5453452f1cf4bc0e6cef56b0f3f153b45b2
Alexander Guttenberger <alex.guttenberger50@gmail.com>
2022-07-11 19:45:12 +0200

    remove unneeded code

--- privacy-component/get-object.js
+++ privacy-component/get-object.js
@@ -6,17 +6,13 @@ exports.handler = async (event) => {
   // Output the event details to CloudWatch Logs.
   console.log("BucketAccess-Event:\n", JSON.stringify(event, null, 2));
 
-  const { S3_ACCESS_POINT, HTTPS_S3_ACCESS_POINT } = process.env;
-  const { id } = event.pathParameters;
+  const Bucket = process.env.S3_ACCESS_POINT;
+  const Key = `${event.pathParameters.id}#test`;
 
-  const params =
-  {
-    Bucket: S3_ACCESS_POINT, // a path to your Bucket
-    Key: `${id}#test` // a key (literally a path to your file)
-  }
+  const result = await s3.getObject({ Bucket, Key }).promise();
+
 
   console.log("S3Params-Event:\n", JSON.stringify({ params }, null, 2));
-  const result = await s3.getObject(params).promise();
 
   // AccessPointName-AccountId.s3-object-lambda.Region.amazonaws.com.
   // const url = https://peng-anon-197938948806.s3-object-lambda.eu-central-1.amazonaws.com
--- privacy-component/get-object.py
+++ privacy-component/get-object.py
@@ -6,19 +6,15 @@ def lambda_handler(event, context):
     s3 = boto3.client('s3')
     s3Bucket = os.environ['S3_ACCESS_POINT']
     s3Key = event["pathParameters"]["id"] + "#" + event["queryStringParameters"]["purposeToken"]
-    
-    print(s3Bucket)
-    print(s3Key)
 
     response = s3.get_object(Bucket=s3Bucket,Key=s3Key)
-
-    print(response)
 
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": response["ContentType"],
             "Content-Length": response["ContentLength"],
+            "Access-Control-Allow-Origin": "*"
         },
         "body": base64.b64encode(response["Body"].read()).decode('utf-8'),
         "isBase64Encoded": True
